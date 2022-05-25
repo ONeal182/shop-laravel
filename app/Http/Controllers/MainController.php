27629be2e57620b797d9bcccf7c080f931fb843f
ONeal <kigat63@gmail.com>
2022-05-25 16:21:22 +0400

    add controller main

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index(){
+        return view('index');
+    }
+    public function categories(){
+        return view('categories');
+    }
+    public function product($product = null){
+        return view('product',['product'=>$product]);
+    }
+    public function category($category = null){
+        return view('category',['category'=>$category]);
+        
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MainController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [MainController::class, 'index']);
+Route::get('/categories', [MainController::class, 'categories']);
+Route::get('/{category}', [MainController::class, 'category']);
 
-Route::get('/categories', function () {
-    return view('categories');
-});
+Route::get('/mobiles/{product?}', [MainController::class, 'product']);
 
-Route::get('/mobiles/iphone_x_64', function () {
-    return view('product');
-});
+
+// Route::get('/categories', function () {
+//     return view('categories');
+// });
+
+// Route::get('/mobiles/iphone_x_64', function () {
+//     return view('product');
+// });
