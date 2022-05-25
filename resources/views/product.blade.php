561b507cbaa07259883f9e14c1dd6236d9ebe6e5
ONeal <kigat63@gmail.com>
2022-05-25 16:17:14 +0400

    cahnge hard url

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -17,19 +17,19 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <a class="navbar-brand" href="http://laravel-diplom-1.rdavydov.ru">Интернет Магазин</a>
+            <a class="navbar-brand" href="">Интернет Магазин</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li ><a href="http://laravel-diplom-1.rdavydov.ru">Все товары</a></li>
-                <li ><a href="http://laravel-diplom-1.rdavydov.ru/categories">Категории</a>
+                <li ><a href="">Все товары</a></li>
+                <li ><a href="/categories">Категории</a>
                 </li>
-                <li ><a href="http://laravel-diplom-1.rdavydov.ru/basket">В корзину</a></li>
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/reset">Сбросить проект в начальное состояние</a></li>
+                <li ><a href="/basket">В корзину</a></li>
+                <li><a href="/reset">Сбросить проект в начальное состояние</a></li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://laravel-diplom-1.rdavydov.ru/admin/home">Панель администратора</a></li>
+                <li><a href="/admin/home">Панель администратора</a></li>
             </ul>
         </div>
     </div>
@@ -39,10 +39,11 @@
 <div class="container">
     <div class="starter-template">
         <h1>iPhone X 64GB</h1>
+        <h2>{{$product}}</h2>
         <p>Цена: <b>71990 руб.</b></p>
-        <img src="http://laravel-diplom-1.rdavydov.ru/storage/products/iphone_x.jpg">
+        <img src="/storage/products/iphone_x.jpg">
         <p>Отличный продвинутый телефон с памятью на 64 gb</p>
-        <a class="btn btn-success" href="http://laravel-diplom-1.rdavydov.ru/basket/1/add">Добавить в корзину</a>
+        <a class="btn btn-success" href="/basket/1/add">Добавить в корзину</a>
     </div>
 </div>
 </body>
