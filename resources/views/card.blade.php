3a3c7587fcb7ae401ec9b46ae53920251f333be7
ONeal <kigat63@gmail.com>
2022-05-27 15:08:31 +0400

    show varible in templayte

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -2,13 +2,18 @@
     <div class="thumbnail">
         <img src="/storage/products/iphone_x.jpg" alt="iPhone X 64GB">
         <div class="caption">
-            <h3>{{$product->name}}</h3>
-            <p>{{$product->price}} руб.</p>
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->price }} руб.</p>
             <p>
-                <a href="{{ route('basket') }}" class="btn btn-primary" role="button">В корзину</a>
-                {{$product->category->name}}
-                
-                <a href="{{ route('product', [$product->category->code, $product->code]) }}" class="btn btn-default" role="button">Подробнее</a>
+            <form action="{{ route('basket-add', $product) }}" method="get">
+                @csrf
+                <button type="submit" class="btn btn-primary" role="button">В корзину</button>
+                {{ $product->category->name }}
+
+                <a href="{{ route('product', [$product->category->code, $product->code]) }}" class="btn btn-default"
+                    role="button">Подробнее</a>
+            </form>
+
             </p>
         </div>
     </div>
