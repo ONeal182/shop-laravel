c0079234098350b4b3bba517939e9df9ce95e924
ONeal <kigat63@gmail.com>
2022-05-30 14:50:50 +0400

    Add pivot func

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,8 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function getPriceForCount($count){
+        return $this->price * $count;
+    }
 }
