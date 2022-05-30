860512ca135b27b3f7167261ab040c3ff98fc16f
ONeal <kigat63@gmail.com>
2022-05-30 14:50:41 +0400

    add pivot func

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -10,14 +10,14 @@ class BasketController extends Controller
     public function basket()
     {
         $orderId = session('orderId');
-        if(is_null($orderId)){
+        if (is_null($orderId)) {
             $order = Order::findOrFail($orderId);
             dd($order);
-        }else{
+        } else {
             $order =  Order::find($orderId);
         }
-        
-        return view('basket', ['order'=>$order]);
+
+        return view('basket', ['order' => $order]);
     }
 
     public function basketPlace()
@@ -36,9 +36,36 @@ class BasketController extends Controller
 
             $order =  Order::find($orderId);
         }
+        if ($order->products->contains($productId)) {
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            $pivotRow->count++;
+            $pivotRow->update();
+        } else {
+            $order->products()->attach($productId);
+        }
 
-        $order->products()->attach($productId);
 
-        return view('basket', ['order'=>$order]);
+        return redirect()->route('basket');
+    }
+
+    public function basketRemove($productId)
+    {
+        $orderId = session('orderId');
+
+        if (is_null($orderId)) {
+            return redirect()->route('basket');
+        }
+        $order =  Order::find($orderId);
+        if ($order->products->contains($productId)) {
+            $pivotRow = $order->products()->where('product_id', $productId)->first()->pivot;
+            if ($pivotRow->count < 2) {
+                $order->products()->detach($productId);
+            } else {
+                $pivotRow->count--;
+                $pivotRow->update();
+            }
+        }
+
+        return redirect()->route('basket');
     }
 }
