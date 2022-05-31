b83e2d3e89344234513d59c509730d912208ce83
ONeal <kigat63@gmail.com>
2022-05-31 15:46:49 +0400

    add user_id in order

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BasketController extends Controller
 {
@@ -49,6 +50,10 @@ class BasketController extends Controller
             $pivotRow->update();
         } else {
             $order->products()->attach($productId);
+        }
+        if(Auth::check()){
+            $order->user_id = Auth::id();
+            $order->save();
         }
         $product = Product::find($productId);
         session()->flash('success', 'Добавлен товар '.$product->name);
