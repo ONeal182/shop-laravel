d6d8085a80c59608dbb8bd1aabcc6164e48bd00e
ONeal <kigat63@gmail.com>
2022-06-04 17:51:06 +0400

    make seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use CategoriesTableSeeder;
 use Illuminate\Database\Seeder;
+use ProductsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(usersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+
+
     }
 }
