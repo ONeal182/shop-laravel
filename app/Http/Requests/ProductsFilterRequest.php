bf99baf43b01b3440e2fb667bdae44eab523891c
ONeal <kigat63@gmail.com>
2022-06-28 14:09:01 +0400

    add request filter

---
+++ app/Http/Requests/ProductsFilterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProductsFilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'price_from' => 'numeric|min:0',
+            'price_to' => 'numeric|min:0'
+
+        ];
+    }
+}
